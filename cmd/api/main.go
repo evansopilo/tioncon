cde432d1f52d9a7b309c158409d1ae96551f3c3d
evansopilo <opiloevans96@gmail.com>
2022-08-05 12:27:31 +0300

    fix: read mongo uri from environment varible

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -36,7 +36,7 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongo_uri"))
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("mongo_uri")))
 	if err != nil {
 		logger.Fatal(err)
 	}
